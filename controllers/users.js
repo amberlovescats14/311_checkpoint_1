7e6faadd8f15bff53e6e1627712e3942d4e433c4
Amber Jones <amberlovescats14@gmail.com>
2019-07-04 08:18:47 -0500

    completed with controllers and errors

--- controllers/users.js
+++ controllers/users.js
@@ -3,29 +3,58 @@ const users = require('../data/index')
 const listUsers = (req, res) => {
   res.json(users)
 }
-const getOne = (req, res) => {
-  const find = users.find(u => u.id == req.params.id)
-  res.json(find)
+const getOne = async (req, res) => {
+  try {
+    const find = await users.find(u => u.id == req.params.id)
+    if(!find) return res.stauts(404).json({msg:`User not found`})
+
+    res.json(find)
+  } catch (error) {
+    console.error(error.message)
+    res.status(500).json({msg:`SERVER ERROR`})
+  }
 }
- const createOne = (req, res) => {
-   let newUser = {
-     id: users.length+1,
-     body: req.body
+ const createOne = async (req, res) => {
+  try {
+    let newUser = {
+      id: users.length+1,
+      body: req.body
+    }
+    if(!req.body) return res.status(400).json({msg: `Please include updated information`})
+   
+    users.push(newUser)
+    res.json(newUser)
+  } catch (error) {
+    console.error(error.message)
+    res.status(500).json({msg: `SERVER ERROR`})
+  }
+ }
+
+ const updateUser = async (req, res) => {
+  try {
+    const find = await users.find(u => u.id == req.params.id)
+    if(!find) return res.status(404).json({msg: `User not found`})
+
+    Object.assign(find, req.body, {id: find.id})
+    res.json(find)
+  } catch (error) {
+    console.error(error.message)
+    res.status(500).json({msg:`SERVER ERROR`})
+  }
+ }
+
+ const deleteUser = async (req, res) => {
+   try {
+  
+     const find = await users.findIndex(u => u.id == req.params.id)
+     if(!find) return res.status(404).json({msg: `User not found`})
+
+     users.splice(find, 1)
+     res.json({msg: `User Deleted`})
+   } catch (error) {
+     console.error(error.message)
+     res.status(500).json({msg: `SERVER ERROR`})
    }
-   users.push(newUser)
-   res.json(newUser)
- }
-
- const updateUser = (req, res) => {
-   const find = users.find(u => u.id == req.params.id)
-   Object.assign(find, req.body, {id: find.id})
-   res.json(find)
- }
-
- const deleteUser = (req, res) => {
-   const find = users.findIndex(u => u.id == req.params.id)
-   users.splice(find, 1)
-   res.json({msg: `User Deleted`})
  }
 
 module.exports = {
